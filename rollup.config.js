7555e01dd1f45a6e4bc620cc8cbc20027744d864
Dante <dengxiang@mobimedical.cn>
2023-04-20 21:25:47 +0800

    feat: 1. 更改 tsconfig 无用编译目标、指定只生成类型文件, 2. 取消使用 @rollup/plugin-json 的依赖(实际未使用)

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import babel from '@rollup/plugin-babel'
 import terser from '@rollup/plugin-terser'
-// import json from '@rollup/plugin-json'
 // import packageConfig from './package.json'
 
 export default commandLineArgs => {
